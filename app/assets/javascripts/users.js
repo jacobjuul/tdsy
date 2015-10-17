3a7bc8067476318c2a7185f6bf2b6e4f1c4fed85
Jacob Juul Christiansen <jacob.juul@gmail.com>
2015-10-17 13:12:59 +0200

    add some javascript

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,2 +1,23 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    var $acceptCheckbox = $('#acceptConditions');
+    var $submit = $('#submit');
+    var $allInputs = $('input');
+
+    // The button should be disabled when loading the site
+    $submit.prop('disabled', true);
+
+    // Enalbe the button if the user has accepted the conditions
+    $acceptCheckbox.on('change', function(event) {
+        if ($acceptCheckbox[0].checked) {
+            $submit.removeClass('disabled');
+            $submit.prop('disabled', false);
+        } else {
+            $submit.addClass('disabled');
+            $submit.prop('disabled', true);
+        }
+    });
+});
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,19 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to 'index'
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :position,
+                                   :email, :telephone, :os, :communication)
+    end
 end
