16c8ad8597dda96fe18ee7b28b4fbc629187b319
Jacob Juul Christiansen <jacob.juul@gmail.com>
2015-10-17 15:02:54 +0200

    Add an ipad

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :position, presence: true
-  validates :telephone, presence: true, uniqueness: true
+  validates :telephone, presence: true, uniqueness: true, length: { is: 8}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                   format: { with: VALID_EMAIL_REGEX },
