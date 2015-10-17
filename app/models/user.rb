3221781c48d879c1bfb50cae3e241c1cee3d632d
Jacob Juul Christiansen <jacob.juul@gmail.com>
2015-10-17 22:20:00 +0200

    Add ability to import csv

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,15 @@
 class User < ActiveRecord::Base
-  before_save { self.email = self.email.downcase }
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :position, presence: true
-  validates :telephone, presence: true, uniqueness: true, length: { is: 8}
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255 },
-  format: { with: VALID_EMAIL_REGEX },
-  uniqueness: { case_sensitive: false }
-  validates :os, presence: true
-  validates :communication, presence: true
+  # before_save { self.email = self.email.downcase }
+  # validates :first_name, presence: true
+  # validates :last_name, presence: true
+  # validates :position, presence: true
+  # validates :telephone, presence: true, uniqueness: true, length: { is: 8}
+  # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  # validates :email, presence: true, length: { maximum: 255 },
+  # format: { with: VALID_EMAIL_REGEX },
+  # uniqueness: { case_sensitive: false }
+  # validates :os, presence: true
+  # validates :communication, presence: true
 
   def self.to_csv(options = {})
     CSV.generate(options) do |csv|
@@ -21,8 +21,21 @@ class User < ActiveRecord::Base
   end
 
   def self.import(file)
+    counter = 0
     CSV.foreach(file.path) do |row|
-      p row
+      if counter > 0
+        row = row[0].split(';')
+        id, first_name, last_name, email, country, profile_text, gender, sales_manager, sales_manager_email = row || ''
+        p first_name
+         user = User.create(id: id, first_name: first_name, last_name: last_name,
+                  email: email, country: country, profile_text: profile_text,
+                  gender: gender, sales_manager: sales_manager)
+        puts user.errors.full_messages.join(".") if user.errors.any?
+        
+      end
+      counter += 1
+
+     
     end
 
   end
